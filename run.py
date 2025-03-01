2ed96cc71f77ecf55682a047698e76521c54e1c5
user <rajeshkoneru29@gmail.com>
2025-03-01 23:34:09 +0530

    final 2 commit

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from audit_tarcker import create_App
-
+import os
 app = create_App()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Use Render’s assigned port
+    app.run(host="0.0.0.0", port=port, debug=False)
